55a14b8d09c544572d5dac9090cc69e98443020b
Alex Kitelinger <bureaucratix@gmail.com>
2019-04-07 16:55:01 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  org_hash = {}
+  data.each do |attr_name, attr_hash|
+    attr_hash.each do |attribute, pigeon_array|
+      pigeon_array.each do |pigeon|
+        org_hash[pigeon] = {} if org_hash[pigeon] == nil
+        org_hash[pigeon][attr_name] = [] if org_hash[pigeon][attr_name] == nil
+        org_hash[pigeon][attr_name].push(attribute.to_s)
+      end
+    end
+  end
+  return org_hash
 end
