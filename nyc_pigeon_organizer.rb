bb429659c0470aa0dc2d3a205c01625160d7c0e8
Ella <michaella.latkovic@gmail.com>
2019-04-20 21:14:39 -0700

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_hash = {}
+
+    data.each do |key, value|
+      value.each do |key2, value2|
+        value2.each do |name|
+          new_hash[name] = {}
+        end
+      end
+    end
+
+    data.each do |key, value|
+      value.each do |key2, value2|
+        new_hash.each do |name, category|
+          category[key] = []
+        end
+      end
+    end
+
+    data.each do |key, value|
+      value.each do |key2, value2|
+        new_hash.each do |name, category|
+          if value2.include?(name)
+            category[key] << key2.to_s
+          end
+        end
+      end
+    end
+
+    new_hash
+  end
