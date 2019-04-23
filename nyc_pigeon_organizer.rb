02f366f6ae822857c077131b969a793764b9b0b4
Rachel <williamsrachelada@gmail.com>
2019-04-22 21:07:53 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, hash|
+    hash.each do |type, arr|
+      arr.each do |name|
+        if !pigeon_list.include?(name)
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][attribute] == nil
+          pigeon_list[name][attribute] = []
+        end
+        pigeon_list[name][attribute].push(type.to_s)
+      end
+    end
+  end
+ pigeon_list
 end
